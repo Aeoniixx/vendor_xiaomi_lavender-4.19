996e98d2ea90837c7194de8e2b855189ff8ea707
Aeoniixx <aeoniixx@gmail.com>
2025-06-15 21:26:16 +0000

    lavender: Declare IMS libs as symlinks during extraction
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1407,3 +1407,7 @@ PRODUCT_PACKAGES += \
     time_daemon \
     xtra-daemon \
     dpmd
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64
